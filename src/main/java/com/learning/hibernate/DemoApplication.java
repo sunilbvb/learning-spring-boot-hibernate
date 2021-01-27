8751347e4991e76cb2cdb1e6f196cd648121c7fa
Sunil <sunil@infiproton.com>
2021-01-27 08:59:35 +0530

    added simple rest service

--- src/main/java/com/learning/hibernate/DemoApplication.java
+++ src/main/java/com/learning/hibernate/DemoApplication.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.learning.hibernate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
